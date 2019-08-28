f3e7c809ab3a3ab5ba288594b1024b846a6f11e2
William Oliveira de Lagos <william.lagos@icloud.com>
2019-08-28 16:09:06 -0300

    Fixed unique constraint and added email and unique validation

--- api/lead/lead.controller.js
+++ api/lead/lead.controller.js
@@ -11,8 +11,13 @@ const Lead = require('./lead.model')
 // --------------- Module Controller
 const LeadCtrl = {
   save: async function (lead) {
-    const saved = await Lead.create(lead)
-    return saved // Returns the created lead
+    const email = lead.email
+    const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/i
+    const validatedEmail = re.test(String(email).toLowerCase())
+    if (!validatedEmail) return { _id: 0, error: 'Invalid email' }
+    const result = await Lead.findOne({ email })
+    if (result) return result
+    else return Lead.create(lead)
   },
   list: async function () {
     return (await Lead.find()).map((lead) => lead) // Gets leads list
